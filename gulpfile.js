8dd22ad83c9e4b4ac53e15a51e2bc24e400fa6f6
SHoogenboom <shoogenboom@competa.com>
2018-09-10 16:58:33 +0200

    made the minify work

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
- 
+
 gulp.task('sass', function () {
   gulp.src('./sass/index.scss')
     .pipe(sass({
@@ -10,10 +10,11 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./'));
 });
 gulp.task('minify-css', function () {
-	return gulp.src('*.css')
-	  .pipe(cleanCSS())
-	  .pipe(gulp.dest('./'));
+  return gulp.src('*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./'));
 });
 gulp.task('watch', function () {
-	gulp.watch('./sass/*.scss', ['sass','minify-css'])
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./index.css', ['minify-css']);
 })
