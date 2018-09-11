2dfb43a2f1f01c757a03f741e20a75ce711362df
SHoogenboom <shoogenboom@competa.com>
2018-09-11 13:36:08 +0200

    sliderworks

--- js/index.js
+++ js/index.js
@@ -1,20 +1,54 @@
-var slider = document.querySelector('.slider__box');
+var slider = document.querySelector('.slider');
+var buttons = document.querySelectorAll('.slider__button');
 var img = 1;
+var interval = false;
 
-function changeImg() {
-	console.log('')
-	switch(img) {
+buttons[0].addEventListener('click', function(){ changeImg(-1); });
+buttons[1].addEventListener('click', imgVal);
+buttons[2].addEventListener('click', function(){ changeImg(1); });
+
+function changeImg( next ) {
+    switch(img) {
 		case 1:
 			slider.src = "img/bier.png";
-			img++;
+			if(next === -1){
+                img = 4;
+            }
+            else{
+                img = 2; 
+            }
 		break;
 		case 2:
 			slider.src = "img/bier!.png";
-			img = 1;
-		break;
+			img += next;
+        break;
+        case 3:
+			slider.src = "img/busje.png";
+			img += next;
+        break;
+        case 4:
+			slider.src = "img/bus.png";
+            if(next === 1){
+                img = 1;
+            }
+            else{
+                img = 3;
+            }
+        break;
 	}
 }
 
-setInterval(function() { 
-				changeImg(); 
-			}, 300);
+function imgVal() {
+    if(interval === true) {
+        clearInterval(imgVal);
+        interval = false;
+    }
+    else {
+        imgVal = setInterval(function() { 
+            changeImg(1); 
+        }, 1000);
+        interval = true;
+    }
+}
+
+imgVal();
